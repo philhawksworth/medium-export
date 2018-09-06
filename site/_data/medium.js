e989713cd9101ffb2c331cb0b9d09eebc561b324
Phil Hawksworth <phil@netify.com>
2018-09-06 13:54:54 +0100

    listing styles

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -1,7 +1,6 @@
 var axios       = require('axios');
 var toJSON = require('xml2js').parseString;
 
-//
 var url = 'https://medium.com/feed/netlify';
 
 module.exports = () => {
@@ -10,10 +9,6 @@ module.exports = () => {
       .then((response) => {
         // turn the feed XML into JSON
         toJSON(response.data, function (err, result) {
-
-          // console.log(result.rss.channel[0].item);
-
-
           // create a path for each item based on Medium's guid URL
           result.rss.channel[0].item.forEach(element => {
             var url = element.link[0].split('/');
