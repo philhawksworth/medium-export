fda982f5d96c1a99baba665cf00922978694769f
Phil Hawksworth <phil@netify.com>
2018-09-06 12:36:52 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const CleanCSS = require("clean-css");
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  return {
+    dir: {
+      input: "site",
+      output: "dist",
+      data: "_data"
+    }
+  };
+
+};
---
+++ site/_data/medium.js
@@ -0,0 +1,25 @@
+var axios       = require('axios');
+var toJSON = require('xml2js').parseString;
+
+//
+var url = 'https://medium.com/feed/netlify';
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then((response) => {
+        // turn the feed XML into JSON
+        toJSON(response.data, function (err, result) {
+          // create a path for each item based on Medium's guid URL
+          result.rss.channel[0].item.forEach(element => {
+            var url = element.link[0].split('/');
+            element.path = url[url.length-1].split('?')[0];
+          });
+          resolve(result.rss.channel[0].item);
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
